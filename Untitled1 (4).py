3bef3cd934d83599f2b948837c45a613e9fca7f2
Lilith1042 <71847979+Lilith1042@users.noreply.github.com>
2021-09-15 00:27:22 +0300

    ex7

---
+++ Untitled1 (4).py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#7
+import turtle
+n=5
+for j in range(20):
+    for i in range(16):
+        turtle.forward(n)
+        turtle.left(10)
+    n=n+5
+
