041079d09f756bfaf0a2fb5a935afec7fc03b85b
Lilith1042 <71847979+Lilith1042@users.noreply.github.com>
2021-09-15 00:36:21 +0300

    ex8

---
+++ Untitled1 (5).py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#8
+import turtle
+n=5
+for i in range(50):
+    for j in range(2):
+        turtle.forward(n)
+        turtle.left(90)
+        n=n+5
+
+
+# In[ ]:
+
+
+
+
