4a1716abed8a5d36baadfde4f7a321902ee57fa9
Lilith1042 <71847979+Lilith1042@users.noreply.github.com>
2021-09-14 23:01:20 +0300

    Add files via upload
    ex2

---
+++ Untitled1.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#2
+import turtle
+
+turtle.shape('turtle')
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.right(90)
+turtle.forward(50)
+turtle.right(90)
+turtle.forward(50)
+
