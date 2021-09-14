a73776c684a3e3ac0f31c92f3a6b18dbce8b08b2
Lilith1042 <71847979+Lilith1042@users.noreply.github.com>
2021-09-15 00:10:25 +0300

    ex6

---
+++ Untitled1 (3).py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+#6
+import turtle
+turtle.shape('turtle')
+for i in range(12):
+    turtle.forward(50)
+    turtle.stamp()
+    turtle.forward(-50)
+    turtle.left(30)
+
+
+# In[ ]:
+
+
+
+
