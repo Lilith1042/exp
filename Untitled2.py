b0b0ea779418f7659a2930b2b7741d81bc489d65
Lilith1042 <71847979+Lilith1042@users.noreply.github.com>
2021-09-18 20:56:07 +0300

    ex13

---
+++ Untitled2.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import turtle as t
+t.color('yellow')
+t.begin_fill()
+t.circle (80)
+t.end_fill()
+
+t.goto(-40, 120)
+t.color('blue')
+t.begin_fill()
+t.circle (10)
+t.end_fill()
+t.penup()
+
+t.goto(40,120)
+t.pendown()
+t.begin_fill()
+t.circle (10)
+t.end_fill()
+t.penup()
+
+
+
+t.goto(0,110)
+t.pendown()
+t.color('black')
+t.width(10)
+t.goto(0,50)
+
+t.penup()
+t.goto(-40,55)
+t.right(90)
+t.pendown()
+t.color('red')
+t.circle(45,180)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
