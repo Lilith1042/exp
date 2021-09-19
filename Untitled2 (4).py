dcbc06eee5e751f6e03677a34f5057c573ae0a37
Lilith1042 <71847979+Lilith1042@users.noreply.github.com>
2021-09-20 01:31:31 +0300

    ex2

---
+++ Untitled2 (4).py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import turtle
+
+turtle.shape('turtle')
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.right(90)
+turtle.forward(50)
+turtle.right(90)
+turtle.forward(50)
+
